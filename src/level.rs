d9007cba45ba8b7fa6af9afb8a60bcdcd8688f8c
Canop <cano.petrole@gmail.com>
2020-02-21 18:56:39 +0100

    fix crash on creating a level

--- src/level.rs
+++ src/level.rs
@@ -11,7 +11,7 @@ use {
 
 /// the description of a level for (de)serialization
 /// and edition (but not game)
-#[derive(Debug, Clone, Default, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Level {
     pub default_cell: Cell,
     pub cells: Vec<Located<Cell>>,
@@ -19,11 +19,22 @@ pub struct Level {
     pub items: Vec<Located<Item>>,
 }
 
+impl Default for Level {
+    fn default() -> Self {
+        Self {
+            default_cell: FIELD,
+            cells: Vec::new(),
+            actors: vec![Actor::new(ActorKind::Lapin, 0, 0)],
+            items: Vec::new(),
+        }
+    }
+}
+
 impl From<&Board> for Level {
     fn from(board: &Board) -> Self {
         let mut level = Level::default();
         level.default_cell = board.cells.default;
-        level.actors.extend(&board.actors);
+        level.actors = board.actors.clone();
         for lc in board.cells.iter() {
             if lc.v != level.default_cell {
                 level.cells.push(lc);
